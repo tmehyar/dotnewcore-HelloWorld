09a2308d731c365f27aaa31085cd111f9e4d6f93
Tamer Mehyar <tamer.mehyar@velir.com>
2020-07-07 10:39:10 -0500

    Initial commit - basic Hello World

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dotnetcore - Hello World!");
+
+             var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app){
+            app.Run(context => {
+                return context.Response.WriteAsync("Hello world");
+            });
+            
+        }
+    }
+}
